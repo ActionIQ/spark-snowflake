497763035942cee01fa87bbeecbbe513de1d23cc
Mitesh Patel <miteshp@live.com>
2023-09-01 18:27:58 -0500

    Fix

---
+++ project/PublishToArtifactory.scala
@@ -0,0 +1,23 @@
+import sbt.Keys._
+import sbt.{Def, _}
+
+// Mirrors aiq - PublishToArtifactory
+object PublishToArtifactory extends AutoPlugin {
+
+  lazy val baseSettings: Seq[Def.Setting[_]] = Seq(
+    publishTo := Some("ArtifactoryRealm".at("https://actioniq.jfrog.io/artifactory/aiq-sbt-local")),
+    publishMavenStyle := true,
+    publishConfiguration := publishConfiguration.value.withOverwrite(isSnapshot.value),
+    publishLocalConfiguration := publishLocalConfiguration.value.withOverwrite(isSnapshot.value),
+    publishM2Configuration := publishM2Configuration.value.withOverwrite(isSnapshot.value),
+    Compile / packageSrc / publishArtifact := false,
+    Compile / packageDoc / publishArtifact := false,
+  )
+
+  override def requires: Plugins = ArtifactoryCredentials
+
+  override def trigger: PluginTrigger = noTrigger
+
+  // a group of settings that are automatically added to projects.
+  override val projectSettings: Seq[Def.Setting[_]] = baseSettings
+}
