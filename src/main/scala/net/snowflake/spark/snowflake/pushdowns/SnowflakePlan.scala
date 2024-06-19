0ef3dac0ca859d03f0f8c1eef49af8fdf34aad3c
Mitesh Patel <miteshp@live.com>
2024-06-18 23:28:17 -0500

    [DPE-334] Use SQLPlan trait #23

--- src/main/scala/net/snowflake/spark/snowflake/pushdowns/SnowflakePlan.scala
+++ src/main/scala/net/snowflake/spark/snowflake/pushdowns/SnowflakePlan.scala
@@ -3,12 +3,12 @@ package net.snowflake.spark.snowflake.pushdowns
 import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.catalyst.InternalRow
 import org.apache.spark.sql.catalyst.expressions.{Attribute, UnsafeProjection}
-import org.apache.spark.sql.execution.SparkPlan
+import org.apache.spark.sql.execution.{SQLPlan, SparkPlan}
 import org.apache.spark.sql.types.{StructField, StructType}
 
 /** SnowflakePlan, with RDD defined by custom query. */
-case class SnowflakePlan(output: Seq[Attribute], rdd: RDD[InternalRow], sql: String)
-    extends SparkPlan {
+case class SnowflakePlan(output: Seq[Attribute], rdd: RDD[InternalRow], override val sql: String)
+    extends SparkPlan with SQLPlan {
 
   override def children: Seq[SparkPlan] = Nil
   protected override def doExecute(): RDD[InternalRow] = {
